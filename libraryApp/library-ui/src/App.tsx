e60c247060d967c3debcddadda97b064d85d2e29
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-12-24 23:28:56 -0600

    frontend - creating homepage I

--- libraryApp/library-ui/src/App.tsx
+++ libraryApp/library-ui/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to the Library UI</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <ExploreTopBooks />
+			<Carousel />
+        </div>
+    );
 }
 
 export default App;
