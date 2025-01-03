69252b1eccba8d7ccf2d834771112365ba1da226
Oscar Santamaria Venegas <osantamaria@gmail.com>
2025-01-02 21:54:53 -0600

    frontend - checkout

--- libraryApp/library-ui/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
+++ libraryApp/library-ui/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
@@ -3,8 +3,33 @@ import BookModel from "../../models/BookModel"
 
 export const CheckoutAndReviewBox: React.FC<{ 
 	book: BookModel | undefined, 
-	mobile: boolean 
+	mobile: boolean,
+	currentLoansCount: number,
+	isAuthenticated: any,
+	isCheckedOut: boolean 
+	checkoutBook: any
 }> = (props) => {
+
+	function buttonRender() {
+		if (props.isAuthenticated) {
+			if (!props.isCheckedOut && props.currentLoansCount < 5) {
+				return (
+					<button onClick={() => props.checkoutBook()} className="btn btn-success btn-lg">Checkout</button>
+				);
+			} else if (props.isCheckedOut) {
+				return (
+					<p><b>Book checked out. Enjoy!</b></p>
+				);
+			} else if (!props.isCheckedOut) {
+				return (
+					<p className="text-danger">Too many books checked out.</p>
+				)
+			}
+		}
+		return (
+			<Link to={'/login'} className="btn btn-success btn-lg">Sign in</Link>
+		);
+	}
 
 	const copiesAvailable = () => props.book && props.book.copiesAvailable && props.book.copiesAvailable > 0;
 
@@ -16,7 +41,7 @@ export const CheckoutAndReviewBox: React.FC<{
 		<div className={props.mobile ? 'card d-flex mt-5' : 'card col-3 container d-flex mb-5'}>
 			<div className="card-body container">
 				<div className="mt-3">
-					<p><b>0/5 </b>books checked out</p>
+					<p><b>{props.currentLoansCount}/5 </b>books checked out</p>
 					<hr />
 					{copiesAvailable() ? displayAvailableText() : displayWaitListText()}
 					<div className="row">
@@ -30,7 +55,7 @@ export const CheckoutAndReviewBox: React.FC<{
 						</p>
 					</div>
 				</div>
-				<Link to='/#' className="btn btn-success btn-lg">Sign in</Link>
+				{buttonRender()}
 				<hr />
 				<p className="mt-3">This number can change until placing order has been complete.</p>
 				<p className="mt-3">Sign in to be able to leave a review</p>
